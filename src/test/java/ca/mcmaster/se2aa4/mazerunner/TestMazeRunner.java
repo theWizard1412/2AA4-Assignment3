98c72351e36e6bad40040a119fe82c7624810dcf
Merlin <merlinliu1591@outlook.com>
2025-04-02 20:23:43 -0400

    added test cases 7,8,9

--- src/test/java/ca/mcmaster/se2aa4/mazerunner/TestMazeRunner.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/TestMazeRunner.java
@@ -66,5 +66,38 @@ public class TestMazeRunner {
     boolean passage = this.maze.isPassage(0, 0);
     assertEquals(passage, false);
   }
+
+  @Test
+  public void testGetX() {
+    position = new Position(3, 4);
+
+   int x = this.position.getX();
+   assertEquals(x, 3);
+  }
+
+  @Test
+  public void testGetY() {
+    position = new Position(3, 5);
+
+    int y = this.position.getY();
+    assertEquals(y, 5);
+  }
   
+  
+  @Test
+  public void testValidatingPath() {
+
+    position = new Position(this.maze.getStart());
+    path = new Path(this.maze, position);
+
+    path.addMovement("F R F 2L 2F R 2F R 2F 2L 4F R 2F R 4F 2L 2F R 4F R 2F R 2F 2L 2F L 2F L 4F R 2F R 2F 2L 4F R 2F R 2F 2L 2F R 2F R 4F R 2F L 2F R 2F L F");
+
+    boolean validate = this.path.validatePath();
+
+    assertEquals(true, validate);
+  }
+     
+  
+
+
 }
