c31f133f777c950e369f33eab4ff610e97165f87
Merlin <merlinliu1591@outlook.com>
2025-04-03 21:49:15 -0400

    implemented command classes

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MoveCommand.java
@@ -0,0 +1,15 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class MoveCommand implements Command {
+  private Position position;
+
+  public MoveCommand(Position position) {
+      this.position = position;
+  }
+
+  @Override
+  public void execute() {
+      position.move();
+  }
+}
+
